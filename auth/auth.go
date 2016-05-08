c55d7630de5bc123a17330dc8f2f3ef2a70eb497
timrourke <timothy.rourke@gmail.com>
2016-05-08 11:07:32 -0500

    Creating basic login and signup handlers

---
+++ auth/auth.go
@@ -0,0 +1,139 @@
+package auth
+
+import (
+	"database/sql"
+	"fmt"
+	jwt_lib "github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"github.com/timrourke/po/database"
+	"github.com/timrourke/po/model"
+	"golang.org/x/crypto/bcrypt"
+	"log"
+	"os"
+	"time"
+)
+
+type Credentials struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+// Authenticates user and returns signed JWT
+func HandleLogin(c *gin.Context) (string, error) {
+	var credentials Credentials
+	c.Bind(&credentials)
+
+	// Look up the user
+	user := model.User{}
+	err := database.DB.Get(&user, "SELECT * FROM user WHERE email = ? LIMIT 1", credentials.Email)
+
+	// Fail immediately if any error occurs
+	if err != nil {
+		log.Println("Error retrieving user:", err)
+		c.JSON(403, gin.H{
+			"status":  "Not authorized",
+			"message": "The email address or password was incorrect.",
+		})
+		c.Abort()
+		return "", err
+	}
+
+	// Compare password with bcrypt
+	err = bcrypt.CompareHashAndPassword([]byte(*user.Password), []byte(credentials.Password))
+	if err != nil {
+		log.Println("Error authenticating user:", err)
+		c.JSON(403, gin.H{
+			"status":  "Not authorized",
+			"message": "The email address or password was incorrect.",
+		})
+		c.Abort()
+		return "", err
+	}
+
+	// TODO: consider storing JWTs in Redis for token revocation
+	// uuid := uuid.NewV4()
+	// fmt.Println("UUID: ", uuid)
+
+	// Build the token
+	token := jwt_lib.New(jwt_lib.GetSigningMethod("HS256"))
+	token.Claims["userid"] = user.GetID()
+	token.Claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		c.JSON(500, gin.H{"message": "Could not log user in"})
+		c.Abort()
+		return "", err
+	}
+	return tokenString, nil
+}
+
+// Create a new user and log them in
+func HandleSignup(c *gin.Context) (string, model.User, error) {
+	var credentials Credentials
+	c.Bind(&credentials)
+
+	// Make sure user doesn't already exist
+	existingUser := model.User{}
+	err := database.DB.Get(&existingUser, "SELECT * FROM user WHERE email = ? LIMIT 1", credentials.Email)
+	if err != nil && err != sql.ErrNoRows {
+		log.Println("Error checking for existing user in signup", err)
+		c.JSON(500, gin.H{"message": "User signup failed"})
+		c.Abort()
+		return "", model.User{}, err
+	}
+	if *existingUser.Email == credentials.Email {
+		log.Println("User already exists, aborting signup")
+		c.JSON(500, gin.H{"message": fmt.Sprintf("A user with the email address %v is already taken", credentials.Email)})
+		c.Abort()
+		return "", model.User{}, fmt.Errorf("A user with the email address %v is already taken", credentials.Email)
+	}
+
+	// Bcrypt the password for the new user
+	bcryptedPassword, err := bcrypt.GenerateFromPassword([]byte(credentials.Password), 11)
+	if err != nil {
+		log.Println("Error bcrypting password for new user", err)
+		c.JSON(500, gin.H{"message": "User signup failed"})
+		c.Abort()
+		return "", model.User{}, err
+	}
+
+	// Create new user
+	passwordString := string(bcryptedPassword[:])
+	user := model.User{
+		Username: &credentials.Username,
+		Email:    &credentials.Email,
+		Password: &passwordString,
+	}
+	result, err := database.DB.Exec("INSERT INTO user (username, email, password) VALUES (?, ?, ?)",
+		user.Username,
+		user.Email,
+		user.Password)
+	if err != nil {
+		log.Println("Error saving new user", err)
+		c.JSON(500, gin.H{"message": "User signup failed"})
+		c.Abort()
+		return "", model.User{}, err
+	}
+	insertId, err := result.LastInsertId()
+	if err != nil {
+		log.Println("Error saving new user and retrieving insert ID", err)
+		c.JSON(500, gin.H{"message": "User signup failed"})
+		c.Abort()
+		return "", model.User{}, err
+	}
+	user.SetID(fmt.Sprintf("%d", insertId))
+
+	// Build the token
+	token := jwt_lib.New(jwt_lib.GetSigningMethod("HS256"))
+	token.Claims["userid"] = user.GetID()
+	token.Claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		c.JSON(500, gin.H{"message": "Could not create token for new user"})
+		c.Abort()
+		return "", model.User{}, err
+	}
+
+	return tokenString, user, nil
+}
--- main.go
+++ main.go
@@ -5,26 +5,42 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	"net/http"
-
+	// "errors"
+	// "github.com/gin-gonic/contrib/jwt"
+	// "github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/manyminds/api2go"
 	"github.com/manyminds/api2go-adapter/gingonic"
+	"github.com/timrourke/po/auth"
+	"github.com/timrourke/po/database"
 	"github.com/timrourke/po/model"
 	"github.com/timrourke/po/resource"
 	"github.com/timrourke/po/storage"
 	"log"
+	"os"
 )
-
-var db *sqlx.DB
 
 func init() {
 	var err error
-	db, err = sqlx.Open("mysql", "golang:golang@tcp(127.0.0.1:3306)/golang?parseTime=true")
+
+	// Load the dotenv files for the application
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Failed to load the dotenv file.")
+	}
+
+	// Attempt to open a sql connection
+	database.DB, err = sqlx.Open("mysql",
+		fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true",
+			os.Getenv("MYSQL_USER"),
+			os.Getenv("MYSQL_PASSWORD"),
+			os.Getenv("MYSQL_DB")))
 	if err != nil {
 		log.Panic(err)
 	}
-	err = db.Ping()
+	err = database.DB.Ping()
 	if err != nil {
 		log.Panic(err)
 	} else {
@@ -35,22 +51,41 @@ func init() {
 func main() {
 	r := gin.Default()
 
+	// Attempt to register sessions using redis
+	// store, _ := sessions.NewRedisStore(10, "tcp", "localhost:6379", os.Getenv("REDIS_PASSWORD"))
+	// r.Use(sessions.Sessions("session", store))
+
 	api := api2go.NewAPIWithRouting(
 		"api",
 		api2go.NewStaticResolver("/"),
 		gingonic.New(r),
 	)
 
-	nounStorage := storage.NewNounStorage(db)
-	verbStorage := storage.NewVerbStorage(db)
-	tensePresIndStorage := storage.NewTensePresIndStorage(db)
-
-	api.AddResource(model.Noun{}, resource.NounResource{NounStorage: nounStorage})
-	api.AddResource(model.Verb{}, resource.VerbResource{VerbStorage: verbStorage})
-	api.AddResource(model.TensePresentIndicative{}, resource.TensePresIndResource{TensePresIndStorage: tensePresIndStorage})
-
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(200, "pong")
+	})
+
+	r.POST("/signup", func(c *gin.Context) {
+		tokenString, newUser, err := auth.HandleSignup(c)
+		if err != nil {
+			log.Println("Error while trying to sign user up", err)
+			c.Abort() // Just to be safe
+			return
+		}
+		c.JSON(200, gin.H{
+			"token": tokenString,
+			"user":  newUser,
+		})
+	})
+
+	r.POST("/login", func(c *gin.Context) {
+		tokenString, err := auth.HandleLogin(c)
+		if err != nil {
+			log.Println("Error while trying to log user in", err)
+			c.Abort() // Just to be safe
+			return
+		}
+		c.JSON(200, gin.H{"token": tokenString})
 	})
 
 	r.GET("/", func(c *gin.Context) {
@@ -65,6 +100,20 @@ func main() {
 		fmt.Println("trying to load path: ", path)
 		http.ServeFile(c.Writer, c.Request, path)
 	})
+
+	// r.Use(func(c *gin.Context) {
+	// 	fmt.Printf("%+v", c.Request.Header)
+	// 	c.JSON(403, c.Request.Header)
+	// 	c.Abort()
+	// })
+
+	nounStorage := storage.NounStorage{}
+	verbStorage := storage.VerbStorage{}
+	tensePresIndStorage := storage.TensePresIndStorage{}
+
+	api.AddResource(model.Noun{}, resource.NounResource{NounStorage: nounStorage})
+	api.AddResource(model.Verb{}, resource.VerbResource{VerbStorage: verbStorage})
+	api.AddResource(model.TensePresentIndicative{}, resource.TensePresIndResource{TensePresIndStorage: tensePresIndStorage})
 
 	r.Run(":8080")
 }
